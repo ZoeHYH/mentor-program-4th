ab09dd11cb2601ab36b88bc15e61fe4388f98e3a
ZoeHoTW <31921198+105405161@users.noreply.github.com>
2020-07-01 17:09:14 +0800

    hw6

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,10 +1,7 @@
 function capitalize(str) {
-	let newStr
-	typeof str[0] === 'string' ? newStr = str[0].toUpperCase() : newStr = str[0]
-	for (let i=1; i<str.length;i++) {
-		newStr += str[i]
-	}
-	return newStr
+  let newStr = typeof str[0] === 'string' ? str[0].toUpperCase() : str[0]
+  for (let i=1; i<str.length;i++) newStr += str[i]
+  return newStr
 }
 
 console.log(capitalize('hello'));
