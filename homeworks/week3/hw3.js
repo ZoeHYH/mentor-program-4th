57ffd7eb9b92bc44391179528d93543aba6aab5f
ZoeHYH <31921198+ZoeHYH@users.noreply.github.com>
2020-07-13 16:49:44 +0800

    Update hw3.js

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,12 +1,11 @@
 function prime(n) {
-  let p = []
-  for (let i = 1;  i < n+1; i++) {
-    if (n % i === 0) p.push(i)
+  const p = [];
+  for (let i = 1; i < n + 1; i+= 1) {
+    if (n % i === 0) p.push(i);
     if (p.length > 2) {
-      console.log('Composite')
-      break
+      console.log('Composite');
+      break;
     }
   }
-  if (p.length < 3) console.log('Prime')
+  if (p.length < 3) console.log('Prime');
 }
-
